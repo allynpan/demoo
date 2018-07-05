b27ac917622c57e495891d2a391c6e018f2ca2d6
Euthpic <490114771@qq.com>
2018-07-05 16:27:12 +0800

    Initial commit

---
+++ src/main/java/com/demo/dao/UserDao.java
@@ -0,0 +1,15 @@
+package com.demo.dao;
+
+import com.demo.model.UserDomain;
+
+import java.util.List;
+
+public interface UserDao {
+
+
+    int insert(UserDomain record);
+
+
+
+    List<UserDomain> selectUsers();
+}
---
+++ src/main/java/com/demo/service/user/UserService.java
@@ -0,0 +1,16 @@
+package com.demo.service.user;
+
+import com.demo.model.UserDomain;
+import com.github.pagehelper.PageInfo;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2018/4/19.
+ */
+public interface UserService {
+
+    int addUser(UserDomain user);
+
+    PageInfo<UserDomain> findAllUser(int pageNum, int pageSize);
+}
